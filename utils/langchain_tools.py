b7944e19e049e48f5a2c8b2f9914c5f9a6788d9c
safa <safahichri77@gmail.com>
2025-07-28 23:56:31 +0200

    Supprimer les anciens fichiers et ajouter les nouveaux

--- utils/langchain_tools.py
+++
@@ -1,9 +0,0 @@
-
-
-from langchain.agents import initialize_agent, Tool
-from langchain_google_community import GoogleSearchAPIWrapper
-from langchain_community.llms import OpenAI
-search = GoogleSearchAPIWrapper()
-
-def run_web_search(query):
-    return search.run(query)
